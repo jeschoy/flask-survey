84dffb8cf3aeff9313495f617ff810c3aaa7f1e8
Jessica Choi <jesymchoi@gmail.com>
2024-05-15 15:23:03 -0700

    completed step 8

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect, request
+from flask import Flask, render_template, flash, redirect, request, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -15,18 +15,19 @@ def first_page():
   """To render the introduction page"""
   return render_template('start.html', survey=survey)
 
-@app.route('/start-survey')
+@app.route('/start-survey', methods=['POST'])
 def start_survey():
   """Clear the responses and start a new survey"""
 
   #To clear the responses
-  responses = []
+  session['responses'] = []
 
   return redirect ('/question/0')
 
 @app.route('/question/<int:num>')
 def show_question(num):
   """To display the current question"""
+  responses = session.get('responses')
 
   #To redirect if there are no responses yet
   if (responses is None):
@@ -54,7 +55,11 @@ def answering_question():
   choice = request.form['answer']
   
   #To add to the responses list
+  responses = session['responses']
   responses.append(choice)
+  session['responses'] = responses
+
+
 
   #To move on to the next question
   return redirect(f'/question/{len(responses)}')
